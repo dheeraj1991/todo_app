81ab0e6b409a2101c9dc8541874fdbb4c87467f8
qplaytechpvtltd <mail@qplaytech.com>
2015-02-13 14:26:32 +0530

    Sort by date

--- task/static/datepicker/js/main.js
+++ task/static/datepicker/js/main.js
@@ -1,3 +1,40 @@
+/**
+* Returns the week number for this date. dowOffset is the day of week the week
+* "starts" on for your locale - it can be from 0 to 6. If dowOffset is 1 (Monday),
+* the week returned is the ISO 8601 week number.
+* @param int dowOffset
+* @return int
+*/
+Date.prototype.getWeek = function (dowOffset) {
+/*getWeek() was developed by Nick Baicoianu at MeanFreePath: http://www.epoch-calendar.com */
+
+dowOffset = typeof(dowOffset) == 'int' ? dowOffset : 0; //default dowOffset to zero
+var newYear = new Date(this.getFullYear(),0,1);
+var day = newYear.getDay() - dowOffset; //the day of week the year begins on
+day = (day >= 0 ? day : day + 7);
+var daynum = Math.floor((this.getTime() - newYear.getTime() -
+(this.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
+var weeknum;
+//if the year starts before the middle of a week
+if(day < 4) {
+weeknum = Math.floor((daynum+day-1)/7) + 1;
+if(weeknum > 52) {
+nYear = new Date(this.getFullYear() + 1,0,1);
+nday = nYear.getDay() - dowOffset;
+nday = nday >= 0 ? nday : nday + 7;
+/*if the next year starts before the middle of
+the week, it is week #1 of that year*/
+weeknum = nday < 4 ? 1 : 53;
+}
+}
+else {
+weeknum = Math.floor((daynum+day-1)/7);
+}
+return weeknum;
+};
+
+
+
 $(document).ready(function() {
   $.ajaxSetup({
              beforeSend: function(xhr, settings) {
@@ -142,4 +179,59 @@ $(document).ready(function() {
             });
     });
 
+    $('.spacing').click(function() {
+      var time = $(this).text()
+      console.log(time);
+      $('.due_date').each(function(index, val){
+        var str = $(this).text();
+        var due = Date.parse(str);
+        var today = Date.parse('today');
+        if (due.getFullYear() == today.getFullYear()){
+          // console.log('Year');
+          if (time=="Month"){
+            console.log('Month');
+            if (due.getMonth() != today.getMonth()){
+              $(this).closest('tr').css('display', 'None');
+            }
+            else{
+              $(this).closest('tr').css('display', 'table-row'); 
+            }
+          }
+          else if(time == "Week"){
+            console.log('Week');
+            if (due.getMonth() == today.getMonth()){
+              if(due.getWeek() != today.getWeek()){
+                $(this).closest('tr').css('display', 'None');
+              }
+              else{
+                $(this).closest('tr').css('display', 'table-row'); 
+              }
+            }
+            else{
+              $(this).closest('tr').css('display', 'None');
+            }
+          }
+          else if(time == "Day"){
+            console.log('Day');
+            if (due.getMonth() == today.getMonth()){
+              if (due.getDate() != today.getDate()){
+                $(this).closest('tr').css('display', 'None');
+              }
+              else{
+                $(this).closest('tr').css('display', 'table-row'); 
+              }
+            }
+            else{
+              $(this).closest('tr').css('display', 'None');
+            }
+          }
+          else if(time == "All"){
+            $(this).closest('tr').css('display', 'table-row'); 
+          }
+        }
+        else {
+          $(this).closest('tr').css('display', 'None');
+        }
+      });
+    });
 });
